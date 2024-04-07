6003f80c21093b68f266156837edc202940eb531
LunaticReisen <longlongbnu@gmail.com>
2024-04-07 19:30:35 +0800

    Remove cache files

--- scripts/Trace.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-class_name Trace
-var startPos : Vector3
-var endPos : Vector3
-var fraction : float
-var startSolid : bool
-var hitCollider : Collision
-var hitPoint : Vector3
-var planeNormal : Vector3
