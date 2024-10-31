ba19942366ac59e6ed538e91ac2e87b3c9e89677
LunaticReisen <30659030+LunaticReisen@users.noreply.github.com>
2024-10-31 17:57:36 +0800

    add direction_base for ladder
    but no code for it hah ,fuck you

--- scripts/entities/funcs/func_ladder.gd
+++ scripts/entities/funcs/func_ladder.gd
@@ -9,10 +9,15 @@ func _init():
     var top_ladder = Marker3D.new()
     add_child(top_ladder)
     top_ladder.add_to_group("MARKER_LADDERTOP")
-    top_ladder.position.y = self.position.y * 2
+    top_ladder.position.y = self.position.y
 
 func _on_ent_entered(ent: Node) -> void:
     print("connect")
 
 func _on_ent_exited(ent: Node) -> void:
     print("disconnect")
+
+#TODO:
+#能在tb设置梯子的方向
+#把shape3D中的Array提取出来，通过梯子方向与比较x和y的大小确定要偏移的方向
+#self.position.x/y += xx; self.get_child(0).position.x/y += xx
