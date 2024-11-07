83fc6ecb0a4da1b3724bef9e6fc866b56258da53
LunaticReisen <30659030+LunaticReisen@users.noreply.github.com>
2024-11-07 18:58:29 +0800

    amost done fuck it

--- scripts/entities/funcs/func_ladder.gd
+++ scripts/entities/funcs/func_ladder.gd
@@ -2,30 +2,36 @@
 class_name FuncLadder
 extends Area3D
 
+@export var direction : int
 @export var angle : float
 
 func _func_godot_apply_properties(props: Dictionary) -> void:
+    direction = props["direction"] as int
     angle = props["angle"] as float
 
 func _init():
     self.add_to_group("AREA_LADDER",true)
     connect("body_entered" , _on_ent_entered)
     connect("body_exited" , _on_ent_exited)
-    init_object()
+    # self.rotation_degrees.y = angle
     var top_ladder = Marker3D.new()
     add_child(top_ladder)
     top_ladder.add_to_group("MARKER_LADDERTOP")
     top_ladder.position.y = self.position.y
 
+func _ready():
+    init_object()
+    pass
+
 func init_object():
-    self.rotate_y(angle)
-    # var collisionShape
-    # if self.get_child(1) is CollisionShape3D :
-    #     collisionShape = self.get_child(1)
-    for child in get_children():
-        if child is CollisionShape3D:
-            child.rotate_y(-angle)
-            print("yes？")
+    if direction == 0 or direction == 1:
+        for child in self.get_children():
+            if child is CollisionShape3D:
+                print(child.rotation_degrees.y)
+                self.rotation_degrees.y = 90
+                child.rotation_degrees.y = -90
+                print("yes？")
+                print(child.rotation_degrees.y)
 
 func _on_ent_entered(ent: Node) -> void:
     print("LADDER:climb")
